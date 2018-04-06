30ab673dff3aa24991494edf7d85d9fb51459367
Winkywow <winkywow@zju.edu.cn>
2018-04-06 18:11:07 +0800

    20180406 commit

---
+++ account/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, render_to_response
+from django import forms
+from django.http import HttpResponseRedirect
+from .models import User
+
+
+def login(request):
+    if request.method == "POST":
+        uf = UserFormLogin(request.POST)
+        if uf.is_valid():
+            username = uf.cleaned_data['username']
+            password = uf.cleaned_data['password']
+            '''
+            user_result = User.objects.filter(username=username, password=password)
+            if len(user_result) > 0:
+                return render_to_response('account/success.html', {'operation': "login"})
+            else:
+                return render(request, 'account/login.html', {
+                    'uf': uf,
+                    'error_message': "The user doesn't exist!",
+                })
+            '''
+            user_result = User.objects.filter(username=username)
+            if len(user_result) > 0:
+                if user_result[0].password == password:
+                    return render_to_response('account/success.html', {'operation': 'login'})
+                else:
+                    return render(request, 'account/login.html', {
+                        'uf': uf,
+                        'error_message': "Please input the correct password!",
+                    })
+            else:
+                return render(request, 'account/login.html', {
+                    'uf': uf,
+                    'error_message': "The user doesn't exist!",
+                })
+    else:
+        uf = UserFormLogin()
+    return render_to_response('account/login.html', {'uf': uf})
+
+
+class UserFormLogin(forms.Form):
+    username = forms.CharField(label='username', max_length=50)
+    password = forms.CharField(label='password', widget=forms.PasswordInput())
---
+++ pyq/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PyqConfig(AppConfig):
+    name = 'pyq'
