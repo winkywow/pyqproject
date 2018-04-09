bff47c8297e65ae0fbee627a86da6b0189d9a422
Winkywow <winkywow@zju.edu.cn>
2018-04-09 21:18:18 +0800

    edit ok

--- pyq/urls.py
+++ pyq/urls.py
@@ -5,4 +5,7 @@ app_name = 'pyq'
 urlpatterns = [
     path('<int:user_id>', views.index, name='index'),
     path('<int:user_id>/post_add', views.post_add, name='post_add'),
+    path('<int:user_id>/<int:post_id>/post_delete', views.post_delete, name='post_delete'),
+    path('<int:user_id>/<int:post_id>/post_edit', views.post_edit, name='post_edit'),
+    path('<int:user_id>/<int:post_id>/post_edit_add', views.post_edit_add, name='post_edit_add'),
 ]
--- pyq/views.py
+++ pyq/views.py
@@ -8,8 +8,54 @@ from django.http import HttpResponse, Http404
 
 # Create your views here.
 def index(request, user_id):
-    posts = list(Post.objects.all())[-10:]
+    posts = list(Post.objects.all())[-1:-7:-1]
     user_login = get_object_or_404(User, pk=user_id)
+    return render(request, 'pyq/index.html', {
+        'posts': posts,
+        'user_login': user_login,
+    })
+
+
+def post_edit(request, user_id, post_id):
+    user_login = get_object_or_404(User, pk=user_id)
+    post_now = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        if user_login == post_now.user_now:
+            return render(request, 'pyq/edit.html', {
+                'post_edit': post_now,
+                'user_login': user_login,
+            })
+    posts = list(Post.objects.all())[-1:-7:-1]
+    return render(request, 'pyq/index.html', {
+        'posts': posts,
+        'user_login': user_login,
+    })
+
+
+def post_edit_add(request, user_id, post_id):
+    user_login = get_object_or_404(User, pk=user_id)
+    post_now = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        if user_login == post_now.user_now:
+            post_nn = PostForms(request.POST)
+            if post_nn.is_valid():
+                context = post_nn.cleaned_data['context']
+                post_now.context = context
+                post_now.save()
+    posts = list(Post.objects.all())[-1:-7:-1]
+    return render(request, 'pyq/index.html', {
+        'posts': posts,
+        'user_login': user_login,
+    })
+
+
+def post_delete(request, user_id, post_id):
+    user_login = get_object_or_404(User, pk=user_id)
+    post_now = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        if user_login == post_now.user_now or user_login.permission:
+            post_now.delete()
+    posts = list(Post.objects.all())[-1:-7:-1]
     return render(request, 'pyq/index.html', {
         'posts': posts,
         'user_login': user_login,
@@ -27,7 +73,7 @@ def post_add(request, user_id):
                 context=context,
             )
             nn_post.save()
-    posts = list(Post.objects.all())[-10:]
+    posts = list(Post.objects.all())[-1:-7:-1]
     return render(request, 'pyq/index.html', {
         'posts': posts,
         'user_login': user_login,
