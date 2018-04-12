559970feee203d4463ad56867afff4f4e2ac995b
Winkywow <winkywow@zju.edu.cn>
2018-04-12 18:21:42 +0800

    restuct ok

--- pyq/views.py
+++ pyq/views.py
@@ -19,6 +19,44 @@ def index(request, user_sid):
     return show(request, user_login)
 
 
+def post_action(request, user_sid, post_id):
+    user_login = get_object_or_404(User, sid=user_sid)
+    if request.method == 'POST':
+        if request.POST['type_change'] == 'add':
+            post_form = PostForms(request.POST)
+            if post_form.is_valid():
+                context = post_form.cleaned_data['context']
+                nn_post = Post(
+                    user_now=user_login,
+                    context=context,
+                )
+                nn_post.save()
+        elif request.POST['type_change'] == 'edit':
+            # post_id = request.POST['post_id']
+            post_now = get_object_or_404(Post, pk=post_id)
+            if user_login == post_now.user_now:
+                return render(request, 'pyq/edit.html', {
+                    'post_edit': post_now,
+                    'user_login': user_login,
+                })
+        elif request.POST['type_change'] == 'delete':
+            # post_id = request.POST['post_id']
+            post_now = get_object_or_404(Post, pk=post_id)
+            if user_login == post_now.user_now or user_login.permission:
+                post_now.delete()
+        elif request.POST['type_change'] == 'edit_add':
+            # post_id = request.POST['post_id']
+            post_now = get_object_or_404(Post, pk=post_id)
+            if user_login == post_now.user_now:
+                post_form = PostForms(request.POST)
+                if post_form.is_valid():
+                    context = post_form.cleaned_data['context']
+                    post_now.context = context
+                    post_now.save()
+    return show(request, user_login)
+
+
+'''
 def post_edit(request, user_sid, post_id):
     user_login = get_object_or_404(User, sid=user_sid)
     post_now = get_object_or_404(Post, pk=post_id)
@@ -58,10 +96,12 @@ def post_add(request, user_sid):
     if request.method == 'POST':
         post_form = PostForms(request.POST)
         if post_form.is_valid():
-            context = post_form.cleaned_data['context']
-            nn_post = Post(
-                user_now=user_login,
-                context=context,
-            )
-            nn_post.save()
+            if request.POST['type_change'] == 'add':
+                context = post_form.cleaned_data['context']
+                nn_post = Post(
+                    user_now=user_login,
+                    context=context,
+                )
+                nn_post.save()
     return show(request, user_login)
+'''
